94aca24f240fc118e6f2a4e1bb568a2e551dfc5f
Matheus Lima <matheus.lima@wexinc.com>
2024-11-11 20:19:47 -0300

    Implement more test cases

--- tddforjava/src/test/java/com/tddforjava/isbntools/ValidateISBNTest.java
+++ tddforjava/src/test/java/com/tddforjava/isbntools/ValidateISBNTest.java
@@ -14,42 +14,85 @@ public class ValidateISBNTest {
 
     @Test
     public void checkAValidISBN10() {
+        // Valid 10 digits ISBNs made up of numbers only
         ValidateISBN validator = new ValidateISBN();
         boolean result = validator.checkISBN("1491950358");
-        assertTrue(result, "First value: Valid 10 digits ISBN");
+        assertTrue(result, "First value: Valid ISBN-10");
         result = validator.checkISBN("1617294543");
-        assertTrue(result, "Second value: Valid 10 digits ISBN");
+        assertTrue(result, "Second value: Valid ISBN-10");
     }
 
     @Test
     public void checkAValidISBN10EndingWithAnX() {
+        // Valid 10 digits ISBNs that end in an X
         ValidateISBN validator = new ValidateISBN();
         boolean result = validator.checkISBN("012000030X");
-        assertTrue(result, "First value: Valid 10 digits ISBN ending with an X");
+        assertTrue(result, "First value: Valid ISBN-10 ending with an X");
     }
 
     @Test
-    public void checkAValidISBN13() {
-        ValidateISBN validator = new ValidateISBN();
-        boolean result = validator.checkISBN("978-1491950357");
-        assertTrue(result, "First value: Valid 13 digits ISBN");
-        result = validator.checkISBN("978-1617294549");
-        assertTrue(result, "Second value: Valid 13 digits ISBN");
-    }
-
-    @Test
-    public void checkAnInvalidISBN() {
+    public void invalidISBN10Digits() {
+        // Invalid 10 digit ISBNs made up of numbers only
         ValidateISBN validator = new ValidateISBN();
         assertThrows(NumberFormatException.class, () -> {
             validator.checkISBN("14919503589");
         });
     }
 
+    @Test
+    public void invalidISBN10DigitsEndingWithAnX() {
+        // Invalid 10 digit ISBNs that end in an X
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("012000430X");
+        assertFalse(result, "First value: Invalid ISBN-10 ending with an X");
+    }
+
     @Test()
-    public void nonNumericISBNsAreNotAllowed() {
+    public void nonNumericISBN10AreNotAllowed() {
+        // Invalid 10 digit ISBNs that aren't numeric
         ValidateISBN validator = new ValidateISBN();
         assertThrows(NumberFormatException.class, () -> {
             validator.checkISBN("helloworld");
         });
     }
+
+    @Test
+    public void checkAValidISBN13() {
+        // Valid 13 digits ISBNs
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("978-1491950357");
+        assertTrue(result, "First value: Valid ISBN-13");
+        result = validator.checkISBN("978-1617294549");
+        assertTrue(result, "Second value: Valid ISBN-13");
+    }
+
+    @Test
+    public void invalidISBN13Digits() {
+        // Invalid 13 digit ISBNs made up of numbers only
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("978-1617294541");
+        assertFalse(result, "First value: Invalid ISBN-13");
+    }
+
+    @Test()
+    public void nonNumericISBN13AreNotAllowed() {
+        // Invalid 13 digit ISBNs that aren't numeric
+        ValidateISBN validator = new ValidateISBN();
+        assertThrows(NumberFormatException.class, () -> {
+            validator.checkISBN("helloworldthi");
+        });
+    }
+
+    @Test()
+    public void ISBNshouldBe10Or13Formats() {
+        // Invalid ISBN that aren't 10 or 13 digits
+        ValidateISBN validator = new ValidateISBN();
+        assertThrows(NumberFormatException.class, () -> {
+            validator.checkISBN("149195035");
+        });
+        assertThrows(NumberFormatException.class, () -> {
+            validator.checkISBN("14919503599");
+        });
+    }
+
 }
